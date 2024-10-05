e158d53ecd0202279553388a47353a98d184eaa2
anacarolcg0 <anacarolcg0@gmail.com>
2024-10-04 21:46:04 -0300

    home feito.

--- cp5/src/app/page.tsx
+++ cp5/src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image"
+import book from "@/img/book.jpg"
+import bookum from "@/img/bookum.jpg"
+import bookdois from "@/img/bookdois.jpg"
 
 export default function Home() {
 
   return (
-    <div> </div>
+    <div className="img-home">
+       <a><Image src={book} alt="book" width={400} height={270} /></a>
+       <a><Image src={bookum} alt="book" width={400} height={270} /></a>
+       <a><Image src={bookdois} alt="book" width={400} height={270} /></a>
+    </div>
 
   )
 }
